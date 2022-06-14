d5dfea5348226cb28be639c8566ade77394c8a77
Masha <39933741+Marechka@users.noreply.github.com>
2022-06-13 23:02:38 -0700

    final

--- src/main/java/org/example/CILab.java
+++ src/main/java/org/example/CILab.java
@@ -2,53 +2,55 @@ package org.example;
 
 
 public class CILab implements CILabInterface {
-        private String myString;
+    private String myString;
 
     @Override
-        public String getString() {
+    public String getString() {
         return myString;
-        }
+    }
 
-        @Override
-        public void setString(String string) {
-            myString = string;
-        }
+    @Override
+    public void setString(String string) {
+        myString = string;
+    }
 
     /**
-     *  We define the usage of capitals in a word to be right when one of the following cases holds:
-     *  All letters in this word are capitals, like "USA".
-     *  All letters in this word are not capitals, like "leetcode".
-     *  Only the first letter in this word is capital, like "Google".
-     * @return  Given a string word, return true if the usage of capitals in it is right.
+     * We define the usage of capitals in a word to be right when one of the following cases holds:
+     * All letters in this word are capitals, like "USA".
+     * All letters in this word are not capitals, like "leetcode".
+     * Only the first letter in this word is capital, like "Google".
+     *
+     * @return Given a string word, return true if the usage of capitals in it is right.
      */
-        @Override
-        public boolean detectCapitalUse() {
-            if (myString == null) return false;
-            int capLetters = 0;
-            int littleLetters = 0;
+    @Override
+    public boolean detectCapitalUse() {
+        if (myString == null) return false;
+        int capLetters = 0;
+        int littleLetters = 0;
 
-           if (Character.isUpperCase(myString.charAt(0))) {
-               capLetters = 1;
-               for (int i = myString.length() - 1; i > 0; i--) {
-                   if (Character.isUpperCase(myString.charAt(i))) {
-                       capLetters++;
-                   }
-               }
-           } else if (Character.isLowerCase(myString.charAt(0))) {
-               littleLetters = 1;
-               for (int i = myString.length() - 1; i > 0; i--) {
-                   if (Character.isLowerCase(myString.charAt(i))) {
-                       littleLetters++;
-                   }
-               }
-           }
+        if (Character.isUpperCase(myString.charAt(0))) {
+            capLetters = 1;
+            for (int i = myString.length() - 1; i > 0; i--) {
+                if (Character.isUpperCase(myString.charAt(i))) {
+                    capLetters++;
 
-           if (capLetters == 1 && littleLetters == myString.length() - 1
-                   || capLetters == myString.length()
-                   || littleLetters == myString.length())
-               return true;
-           else
-               return false;
+                }
+            }
         }
+        if (capLetters == 1 || Character.isLowerCase(myString.charAt(0))) {
+            littleLetters = 1;
+            for (int i = myString.length() - 1; i > 0; i--) {
+                if (Character.isLowerCase(myString.charAt(i))) {
+                    littleLetters++;
+                }
+            }
+        }
+        if (capLetters == 1 && littleLetters == myString.length() - 1
+                || capLetters == myString.length()
+                || littleLetters == myString.length())
+            return true;
+        else
+            return false;
 
+    }
 }
